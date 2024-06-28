9f8d3f9d72a25aa15bcb8f2d0554f7b116cf5cba
harijoshi07 <hariojoshi@gmail.com>
2024-06-28 22:07:29 +0545

    initial commit

---
+++ src/my_robot_controller/my_robot_controller/first_node.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import rclpy
+
+from rclpy.node import Node
+
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("first__node")
+        self.get_logger().info("hey ROS2")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+
+if __name__ == "__main__":
+    main()
